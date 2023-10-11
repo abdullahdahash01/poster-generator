70cc15b937a7a8679a0ee84067e81639d4e68bea
abdullahdahash01 <abdullahdahash89@gmail.com>
2023-10-11 10:31:57 +0300

    optimizing performance

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
 const express = require("express");
 const { chromium } = require("playwright");
 const jsdom = require("jsdom");
-const path = require('path');
-const html = require('./htmlContent')
+const path = require("path");
+const html = require("./htmlContent");
 const { JSDOM } = jsdom;
-const fs = require('fs').promises
+const fs = require("fs").promises;
 
 const app = express();
-const port = 3000
-const baseUrl = `http://localhost:${port}`
+const port = 3000;
+const baseUrl = `http://localhost:${port}`;
+let tempScale;
 
-app.use('/poster', express.static(path.join(__dirname, 'posters')))
+app.use("/poster", express.static(path.join(__dirname, "posters")));
 
 app.get("/generate-poster", async (req, res) => {
   try {
-    const scale = parseFloat(req.query.scale) || 1;
+    let scale = parseFloat(req.query.scale) || 1;
+
+    if (scale === tempScale) {
+      //send the response as a json
+      res.status(200).json({
+        status: "ok",
+        data: {
+          poster: `${baseUrl}/poster/screenShot.png`,
+        },
+      });
+      
+      return;
+    }
+
+    tempScale = scale
 
     const width = 900 * scale;
     const height = 700 * scale;
@@ -27,36 +42,35 @@ app.get("/generate-poster", async (req, res) => {
     // use JSDOM to get access to document object
     const dom = new JSDOM(html);
     const htmlElement = dom.window.document.querySelector("html");
-    const computedStyle = dom.window.getComputedStyle(htmlElement)
-  
+    const computedStyle = dom.window.getComputedStyle(htmlElement);
+
     // get and set the fontSize of the htmlElement
-    const font = parseFloat(computedStyle.getPropertyValue('font-size')) * scale
-    htmlElement.setAttribute("style",`font-size: ${font}%`)
-   
+    const font =
+      parseFloat(computedStyle.getPropertyValue("font-size")) * scale;
+    htmlElement.setAttribute("style", `font-size: ${font}%`);
+
     // serialize the dom
     const modifiedHTML = dom.serialize();
-  
+
     await page.setContent(modifiedHTML);
     const screenShot = await page.screenshot();
 
-    // write the screenShot to /posters directory 
-    await fs.writeFile('./posters/screenshot.png', screenShot)
+    // write the screenShot to /posters directory
+    await fs.writeFile("./posters/screenshot.png", screenShot);
     await browser.close();
 
     //send the response as a json
     res.status(200).json({
-      status:'ok',
-      data:{
-        poster: `${baseUrl}/poster/screenShot.png`
-      }
-    })
-
+      status: "ok",
+      data: {
+        poster: `${baseUrl}/poster/screenShot.png`,
+      },
+    });
   } catch (error) {
     console.error("Error generating poster:", error);
-    res.status(500).send("Error generating poster");  
+    res.status(500).send("Error generating poster");
   }
 });
-
 
 app.listen(3000, () => {
   console.log(`server running on port ${port}`);
